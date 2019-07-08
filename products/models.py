4de293f6d785e8eb6496ee34ef400a51cd4d5e1a
Kotodian <417350372@qq.com>
2019-07-08 11:15:44 +0800

    发布产品的逻辑层和数据层

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(default='抖音:脑瘫聚集地', max_length=50)
+    intro = models.CharField(default='这里写介绍', max_length=50)
+    url = models.CharField(default='Http://', max_length=50)
+    icon = models.ImageField(default='default_icon.png', upload_to='images/')
+    image = models.ImageField(default='default_image.png', upload_to='images/')
+
+    votes = models.IntegerField(default=1)
+    pub_date = models.DateField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- products/views.py
+++ products/views.py
@@ -1,20 +1,36 @@
-from django.shortcuts import render
-
-
 # Create your views here.
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .models import Product
 
 
 def product_list(request):
     return render(request, 'product_list.html')
 
 
+@login_required
 def publish(request):
-    if request.method=='GET':
-        return render(request,'publish.html')
-    elif request.method=='POST':
-        app_name=request.POST['APPName']
-        intro=request.POST['introduction']
-        linkAddress=request.POST['linkAddress']
-        icon=request.POST['APPIcon']
-        img=request.POST['pic']
-        return render(request,'publish.html')
+    if request.method == 'GET':
+        return render(request, 'publish.html')
+    elif request.method == 'POST':
+        app_name = request.POST['APPName']
+        intro = request.POST['introduction']
+        linkAddress = request.POST['linkAddress']
+        try:
+            icon = request.FILES['APPIcon']
+            img = request.FILES['pic']
+        except Exception as err:
+            return render(request,'publish.html',{'错误':'请上传图片'})
+        product = Product()
+        product.title = app_name
+        product.intro = intro
+        product.url = linkAddress
+        product.icon = icon
+        product.image = img
+
+        product.pub_date = timezone.datetime.now()
+        product.hunter = request.user
+
+        product.save()
+        return redirect('主页')

38d8c112ff91379cf15845bfacb6555994bdfc4e
Kotodian <417350372@qq.com>
2019-07-08 10:06:54 +0800

    登录 退出

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('signup/',views.signup,name='注册页面')
+    path('signup/',views.signup,name='注册页面'),
+    path('login/',views.login,name='登录页面'),
+    path('logout/',views.logout,name='退出页面')
 ]
--- account/views.py
+++ account/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render,redirect
+from django.contrib import auth
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 
 
+# 逻辑层
 # Create your views here.
+
+
 def signup(request):
     if request.method == 'GET':
         return render(request, 'signup.html')
@@ -15,7 +19,25 @@ def signup(request):
             return render(request, 'signup.html', {'用户名错误': '该用户名已存在'})
         except User.DoesNotExist:
             if password == password2:
-                User.objects.create(username=user_name,password=password)
+                User.objects.create_user(username=user_name, password=password)
                 return redirect('主页')
             else:
                 return render(request, 'signup.html', {'密码错误': '两次输入的密码不一致!'})
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        user_name = request.POST['username']
+        pass_word = request.POST['password']
+        user = auth.authenticate(username=user_name, password=pass_word)
+        if user is None:
+            return render(request, 'login.html', {'错误': '用户名或密码错误'})
+        else:
+            auth.login(request, user)
+            return redirect('主页')
+def logout(request):
+    if request.method=='POST':
+        auth.logout(request)
+        return redirect('主页')
